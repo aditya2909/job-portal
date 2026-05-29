908fed0c503c9fff1fcf5366483a34474a0988cd
Aditya Jaiswal <86339116+aditya2909@users.noreply.github.com>
2026-05-29 14:44:56 +0530

    Implement CORS middleware with allowed origins

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,24 @@ const analyticsRoutes = require("./routes/analyticsRoute");
 const app = express();
 
 //Middleware to handle CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://job-portal-new-lilac.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+    credentials: true,
+  })
 );
 
 //Connect Database
